587b5f60a093efaffc4c4dbf1b13d2edff6e48c8
Miriam <miajardine1@gmail.com>
2022-11-02 19:25:59 -0600

    Learning Py

---
+++ MayaPythonProject/pyVariablesNotes.py
@@ -0,0 +1,18 @@
+# string ("str") : "timothy" 'timothy' """timothy"""
+
+# integer (int) : 89, 90, 45 -567, 0
+
+# float (float) : 25, 23.601, 963.5
+
+# boolean (bool) : 1 or 0 True or False
+
+# collections
+# -list : [tim", "kim", "bob"] list((34, 56, 78)) ORDER CHANGEABLE DUPLICATES
+# -tuple : ("tim", "kim", "bob") tuple((34, 56, 78)) ORDER UNCHANGEABLE DUPLICATES
+# -set : {tim", "kim", "bob"} set((34, 56, 78)) UNORDERED UNCHANGEABLE (can remove) NO DUPLICATES
+# -dictionary : pet = {"type" : "cat", "color" : "orange", "personality" : "annoying"}
+#               pet = {"type" : "cat", "color" : "orange", "personality" : "annoying"}
+#               pet["type"] = "dog"
+#               print (pet["type"])
+# -NoneType (None) : None
+
