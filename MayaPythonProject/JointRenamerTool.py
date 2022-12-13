dc0ecac642c032a098f0512ec110c601bb6fa890
Miriam <miajardine1@gmail.com>
2022-12-12 17:38:40 -0700

    JointRenamerTool

---
+++ MayaPythonProject/JointRenamerTool.py
@@ -0,0 +1,19 @@
+import maya.cmds as cmds
+
+txt = input()
+sels = cmds.ls(sl=True)
+
+for i in range(len(sels)):
+    count = txt.count('#')
+    x = txt.find("#" * count)
+    if x == 0:
+        cmds.error('# must be consecutive.')
+    if count == 0:
+        cmds.error('Must enter # as digit')
+
+    y = txt.rpartition("#" * count)
+    num = i+1
+    str_num = str(num)
+    digit = str_num.zfill(count)
+    middle = txt.replace("#"*count, digit)
+    print(middle)
